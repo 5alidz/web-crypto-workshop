02a9e0c259a88336357f06f2f69f07b0c90db08b
khaled <kzack-dev@hotmail.com>
2021-06-28 06:18:02 +0300

    fix typo

--- pages/lab/3.tsx
+++ pages/lab/3.tsx
@@ -1,6 +1,94 @@
 import { base64ToByteArray, byteArrayToBase64 } from 'lib/utils';
 import { useState } from 'react';
 
+const lib = (() => {
+  return {
+    async generateKeyPair() {
+      // generate key pair
+      const keyPair = await crypto.subtle.generateKey(
+        {
+          name: 'RSASSA-PKCS1-v1_5',
+          modulusLength: 2048,
+          publicExponent: new Uint8Array([1, 0, 1]), // 65537
+          hash: { name: 'SHA-256' },
+        },
+        true,
+        ['sign', 'verify']
+      );
+      // export public key to view it
+      const spkiBuffer = await crypto.subtle.exportKey('spki', keyPair.publicKey);
+      const spkiBytes = new Uint8Array(spkiBuffer);
+      const spkiEncoded = byteArrayToBase64(spkiBytes);
+
+      // export private key to view it
+      const pkcs8Buffer = await crypto.subtle.exportKey('pkcs8', keyPair.privateKey);
+      const pkcs8Bytes = new Uint8Array(pkcs8Buffer);
+      const pkcs8Encoded = byteArrayToBase64(pkcs8Bytes);
+      return {
+        pkcs8Encoded,
+        spkiEncoded,
+      };
+    },
+    async sign(file: File, privateKey: string) {
+      let signature: null | string = null;
+      const pkcs8Bytes = base64ToByteArray(privateKey);
+      // re-create the private key from the byte array
+      const pkcs8 = await crypto.subtle.importKey(
+        'pkcs8',
+        pkcs8Bytes,
+        { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+        false,
+        ['sign']
+      );
+      const reader = new FileReader();
+      reader.onload = async function (e) {
+        const blob = reader.result;
+        if (blob && typeof blob != 'string') {
+          const documentBytes = new Uint8Array(blob);
+          const signatureBuffer = await crypto.subtle.sign(
+            { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+            pkcs8,
+            documentBytes
+          );
+          const signatureBytes = new Uint8Array(signatureBuffer);
+          const singatureEncoded = byteArrayToBase64(signatureBytes);
+          signature = singatureEncoded;
+        }
+      };
+      reader.readAsArrayBuffer(file);
+      return signature;
+    },
+    async verify(file: File, publicKey: string, signature: string) {
+      let isValid = false;
+      // now anyone with the public key can verify that the file was signed by the private key author
+      const spkiBytes = base64ToByteArray(publicKey);
+      // re-create the public the key from the base64 encoded string
+      const spki = await crypto.subtle.importKey(
+        'spki',
+        spkiBytes,
+        { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+        false,
+        ['verify']
+      );
+      const reader = new FileReader();
+      reader.onload = async function (e) {
+        const blob = e.target?.result;
+        if (blob && typeof blob != 'string') {
+          const documentBytes = new Uint8Array(blob);
+          isValid = await crypto.subtle.verify(
+            { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+            spki,
+            base64ToByteArray(signature),
+            documentBytes
+          );
+        }
+      };
+      if (file) reader.readAsArrayBuffer(file);
+      return isValid;
+    },
+  };
+})();
+
 export default function DigitalSigningAndVerification() {
   const [publicKey, setPublicKey] = useState('');
   const [privateKey, setPrivateKey] = useState('');
@@ -16,27 +104,8 @@ export default function DigitalSigningAndVerification() {
           className='bg-gray-200 tracking-wide uppercase px-4 py-1 font-semibold'
           onClick={() => {
             (async () => {
-              // generate key pair
-              const keyPair = await crypto.subtle.generateKey(
-                {
-                  name: 'RSASSA-PKCS1-v1_5',
-                  modulusLength: 2048,
-                  publicExponent: new Uint8Array([1, 0, 1]), // 65537
-                  hash: { name: 'SHA-256' },
-                },
-                true,
-                ['sign', 'verify']
-              );
-              // export public key to view it
-              const spkiBuffer = await crypto.subtle.exportKey('spki', keyPair.publicKey);
-              const spkiBytes = new Uint8Array(spkiBuffer);
-              const spkiEncoded = byteArrayToBase64(spkiBytes);
+              const { pkcs8Encoded, spkiEncoded } = await lib.generateKeyPair();
               setPublicKey(spkiEncoded);
-
-              // export private key to view it
-              const pkcs8Buffer = await crypto.subtle.exportKey('pkcs8', keyPair.privateKey);
-              const pkcs8Bytes = new Uint8Array(pkcs8Buffer);
-              const pkcs8Encoded = byteArrayToBase64(pkcs8Bytes);
               setPrivateKey(pkcs8Encoded);
             })();
           }}
@@ -75,32 +144,9 @@ export default function DigitalSigningAndVerification() {
             onClick={() => {
               (async () => {
                 // singing with the private key
-                const pkcs8Bytes = base64ToByteArray(publicKey);
-                // re-create the private key from the byte array
-                const pkcs8 = await crypto.subtle.importKey(
-                  'pkcs8',
-                  pkcs8Bytes,
-                  { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-                  false,
-                  ['sign']
-                );
-                const reader = new FileReader();
-                reader.onload = async function (e) {
-                  const blob = e.target?.result;
-                  if (blob && typeof blob != 'string') {
-                    const documentBytes = new Uint8Array(blob);
-                    const signatureBuffer = await crypto.subtle.sign(
-                      { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-                      pkcs8,
-                      documentBytes
-                    );
-                    const signatureBytes = new Uint8Array(signatureBuffer);
-                    const singatureEncoded = byteArrayToBase64(signatureBytes);
-                    setSignature(singatureEncoded);
-                  }
-                };
                 if (file) {
-                  reader.readAsArrayBuffer(file);
+                  const signature = await lib.sign(file, privateKey);
+                  if (signature) setSignature(signature);
                 }
               })();
             }}
@@ -111,31 +157,9 @@ export default function DigitalSigningAndVerification() {
             className='bg-gray-200 tracking-wide uppercase px-4 py-1 font-semibold'
             onClick={() => {
               (async () => {
-                // now anyone with the public key can verify that the file was signed by the private key author
-                const spkiBytes = base64ToByteArray(publicKey);
-                // re-create the public the key from the base64 encoded string
-                const spki = await crypto.subtle.importKey(
-                  'spki',
-                  spkiBytes,
-                  { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-                  false,
-                  ['verify']
-                );
-                const reader = new FileReader();
-                reader.onload = async function (e) {
-                  const blob = e.target?.result;
-                  if (blob && typeof blob != 'string') {
-                    const documentBytes = new Uint8Array(blob);
-                    const isValid = await crypto.subtle.verify(
-                      { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-                      spki,
-                      base64ToByteArray(signature),
-                      documentBytes
-                    );
-                    setIsValidSignature(isValid);
-                  }
-                };
-                if (file) reader.readAsArrayBuffer(file);
+                if (file) {
+                  setIsValidSignature(await lib.verify(file, publicKey, signature));
+                }
               })();
             }}
           >
